4301e012db2b8080637385bdce61b13216bc6d00
Jhzlo <kjh0010703@naver.com>
2025-05-25 03:03:12 +0900

    refactor: transactional 추가

--- src/main/kotlin/com/dku/objectstorage/storage/domain/service/FileMetadataService.kt
+++ src/main/kotlin/com/dku/objectstorage/storage/domain/service/FileMetadataService.kt
@@ -3,11 +3,13 @@ package com.dku.objectstorage.storage.domain.service
 import com.dku.objectstorage.storage.domain.entity.FileMetadata
 import com.dku.objectstorage.storage.domain.repository.FileMetadataRepository
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class FileMetadataService(
     private val fileMetadataRepository: FileMetadataRepository
 ) {
+    @Transactional
     fun save(metadata: FileMetadata): FileMetadata {
         return fileMetadataRepository.save(metadata)
     }
