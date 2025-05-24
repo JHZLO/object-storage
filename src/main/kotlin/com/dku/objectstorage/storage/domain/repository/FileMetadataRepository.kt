16be990085598654c17e09c155d366345d981222
Jhzlo <kjh0010703@naver.com>
2025-05-25 02:46:54 +0900

    feat: FileMetaData 레포지토리 생성

---
+++ src/main/kotlin/com/dku/objectstorage/storage/domain/repository/FileMetadataRepository.kt
@@ -0,0 +1,6 @@
+package com.dku.objectstorage.storage.domain.repository
+
+import com.dku.objectstorage.storage.domain.entity.FileMetadata
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface FileMetadataRepository : JpaRepository<FileMetadata, String>
