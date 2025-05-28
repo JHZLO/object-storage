74679167a5036337297358170f2cd69d826ac35b
Jhzlo <kjh0010703@naver.com>
2025-05-28 09:50:27 +0900

    feat: FileMetaData service 로직 작성

--- src/main/kotlin/com/dku/objectstorage/storage/domain/service/FileMetadataService.kt
+++ src/main/kotlin/com/dku/objectstorage/storage/domain/service/FileMetadataService.kt
@@ -2,6 +2,7 @@ package com.dku.objectstorage.storage.domain.service
 
 import com.dku.objectstorage.storage.domain.entity.FileMetadata
 import com.dku.objectstorage.storage.domain.repository.FileMetadataRepository
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -12,5 +13,10 @@ class FileMetadataService(
     @Transactional
     fun save(metadata: FileMetadata): FileMetadata {
         return fileMetadataRepository.save(metadata)
+    }
+
+    @Transactional(readOnly = true)
+    fun findById(id: String): FileMetadata? {
+        return fileMetadataRepository.findByIdOrNull(id);
     }
 }
