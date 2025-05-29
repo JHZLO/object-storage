2f4e4daad2a292ddd8345bc4a8ba6f0542033b42
Jhzlo <kjh0010703@naver.com>
2025-05-30 02:14:20 +0900

    feat: file 정보 리스트 조회 service 로직 구현

--- src/main/kotlin/com/dku/objectstorage/storage/domain/repository/FileMetadataRepository.kt
+++ src/main/kotlin/com/dku/objectstorage/storage/domain/repository/FileMetadataRepository.kt
@@ -3,4 +3,6 @@ package com.dku.objectstorage.storage.domain.repository
 import com.dku.objectstorage.storage.domain.entity.FileMetadata
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface FileMetadataRepository : JpaRepository<FileMetadata, String>
+interface FileMetadataRepository : JpaRepository<FileMetadata, String> {
+    fun findAllByUploaderId(uploaderId: Long): List<FileMetadata>
+}
--- src/main/kotlin/com/dku/objectstorage/storage/domain/service/FileMetadataService.kt
+++ src/main/kotlin/com/dku/objectstorage/storage/domain/service/FileMetadataService.kt
@@ -2,6 +2,7 @@ package com.dku.objectstorage.storage.domain.service
 
 import com.dku.objectstorage.storage.domain.entity.FileMetadata
 import com.dku.objectstorage.storage.domain.repository.FileMetadataRepository
+import org.apache.coyote.BadRequestException
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -16,7 +17,17 @@ class FileMetadataService(
     }
 
     @Transactional(readOnly = true)
+    fun getById(id: String): FileMetadata {
+        return findById(id) ?: throw BadRequestException("objectstorage.storage.file-not-found")
+    }
+
+    @Transactional(readOnly = true)
     fun findById(id: String): FileMetadata? {
         return fileMetadataRepository.findByIdOrNull(id);
+    }
+
+    @Transactional(readOnly = true)
+    fun findByUploaderId(uploaderId: Long): List<FileMetadata> {
+        return fileMetadataRepository.findAllByUploaderId(uploaderId)
     }
 }
